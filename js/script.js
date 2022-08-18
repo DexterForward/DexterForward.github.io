f1755f5e085c342189a52375c3f3bb9cd6a9cb0a
Vladimir <forward_78@inbox.ru>
2022-08-18 14:42:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $(this).find("textarea").val("");
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
+
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
@@ -20,21 +37,4 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
-});
-
-$(document).ready(function(){
-    $('form').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "/mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-            $(this).find("textarea").val("");
-
-            $('form').trigger('reset');
-        });
-        return false;
-    });
 });
