9aa50ca8d3ece7e7be6aebf594b5156e9da23103
Vladimir <forward_78@inbox.ru>
2022-08-18 13:32:55 +0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+      menuLink = document.querySelector('.menu__list');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuLink.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+$(document).ready(function(){
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $(this).find("textarea").val("");
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
